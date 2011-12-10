4208499d4d04e853d33e943d2df74b35a10b62b1
dem <dvarkin@gmail.com>
2011-12-10 13:18:54 +0200

    add macroses for get value from proplists in param modules

--- include/erl_fun_name.hrl
+++ include/erl_fun_name.hrl
@@ -6,3 +6,9 @@
 -define(ERL_FUN_STRING, '__erl_fun_string__').
 %% function arity
 -define(ERL_FUN_ARITY, '__erl_fun_arity__'). 
+
+-define(GET_BY_NAME(Default), proplists:get_value(?ERL_FUN_BINARY, Args, Default)).
+-define(GET_BY_NAME, ?GET_BY_NAME(undefined)).
+-define(SET(Key, Value), ?MODULE:new(lists:keystore(Key, 1, Args, {Key, Value}))).
+-define(SET(Value), ?SET(?FUNCTION_BINARY, Value)).
+
