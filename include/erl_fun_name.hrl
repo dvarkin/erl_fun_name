86d89b9ff0d0d3547cf39fd458a00b7b8515d455
dem <dvarkin@gmail.com>
2011-12-10 15:57:28 +0200

    fix set macro

--- include/erl_fun_name.hrl
+++ include/erl_fun_name.hrl
@@ -10,5 +10,5 @@
 -define(GET_BY_NAME(Default), proplists:get_value(?ERL_FUN_BINARY, Args, Default)).
 -define(GET_BY_NAME, ?GET_BY_NAME(undefined)).
 -define(SET(Key, Value), ?MODULE:new(lists:keystore(Key, 1, Args, {Key, Value}))).
--define(SET(Value), ?SET(?FUNCTION_BINARY, Value)).
+-define(SET(Value), ?SET(?ERL_FUN_BINARY, Value)).
 
