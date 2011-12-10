402cd29cf689e01915186daa92dff154e76461d7
dem <dvarkin@gmail.com>
2011-12-10 12:23:31 +0200

    add repo files

---
+++ src/erl_fun_name.erl
@@ -0,0 +1,28 @@
+-module(erl_fun_name).
+-export([parse_transform/2]). 
+
+-include("erl_fun_name.hrl").
+
+
+parse_transform(AST, _Options) -> [parse(T) || T <- AST]. 
+
+parse({function, _, FName, FArity, _} = T) ->
+	erl_syntax_lib:map(fun(TE) 
+						  -> parsemacro(FName, FArity, TE) end, T); 
+parse(T) -> T. 
+
+parsemacro(FName, FArity, T) ->
+	R = case erl_syntax:type(T) of 
+			atom -> 
+				case erl_syntax:atom_value(T) of 
+					?ERL_FUN_ATOM -> erl_syntax:atom(FName);
+					?ERL_FUN_STRING -> erl_syntax:string(FName);
+					?ERL_FUN_BINARY ->
+						erl_syntax:binary([erl_syntax:binary_field(erl_syntax:integer(X)) || X <- atom_to_list(FName)]);
+                    ?ERL_FUN_ARITY -> erl_syntax:integer(FArity);
+                    _ -> T
+                end; 
+			_ -> T
+					 
+		end,
+	erl_syntax:revert(R). 
---
+++ src/erl_fun_name.hrl
@@ -0,0 +1,8 @@
+%% function name as atom
+-define(ERL_FUN_ATOM, '__erl_fun_atom__').
+%% function name as binary
+-define(ERL_FUN_BINARY, '__erl_fun_binary__').
+%% function name as string
+-define(ERL_FUN_STRING, '__erl_fun_string__').
+%% function arity
+-define(ERL_FUN_ARITY, '__erl_fun_arity__'). 
